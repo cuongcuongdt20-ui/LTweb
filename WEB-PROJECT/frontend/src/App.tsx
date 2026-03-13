e75f337a4009d4a2c157ae004828b21726bc5f01
XuanQuang1301 <quangxuan1301@gmail.com>
2026-03-13 22:58:09 +0700

    Cập nhật giao diện Light Mode và fix lỗi Dashboard

--- WEB-PROJECT/frontend/src/App.tsx
+++ WEB-PROJECT/frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import {Routes, Route, Navigate} from 'react-router-dom'; 
 import Login from './pages/Login';
 import Register from './pages/Register';
-
+import Layout from './components/Layout'; 
+import Dashboard from './pages/Dashboard';
 function App(){
   return (
     <Routes> 
-      <Route path = "/" element = {<Navigate to="/login" /> } /> 
-      <Route path = "/login" element = {<Login /> } /> 
       <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route element ={<Layout />}>
+        <Route path="/" element = {<Dashboard /> } />
+        
+      </Route>
     </Routes>
   )
 }
